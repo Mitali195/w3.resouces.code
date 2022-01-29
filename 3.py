cd191d5f68a2f759761b373e8fc0d2275f8d5134
Mitali Jadhav <67255177+Mitali195@users.noreply.github.com>
2022-01-29 22:10:55 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+# Write a Python program which accepts the radius of a circle from the user and compute the area.
+
+from cmath import pi
+r = float(input('Enter the radius of the circle:')
+print('The radius of circle is:'+ str(r) + 'is:' + str(pi*r**2)
